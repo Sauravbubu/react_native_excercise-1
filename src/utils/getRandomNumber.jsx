31cfd2993b17757f6713ccc7f669a6e8d24083ff
saurav behera <saurav.behera@logituit.com>
2023-09-22 10:48:41 +0530

    added logic

--- src/utils/getRandomNumber.jsx
+++ src/utils/getRandomNumber.jsx
@@ -1,23 +1,20 @@
 import {useState} from 'react';
 
-export const useGenerateRandomNumber = ({selectedNumber}) => {
+export const useGenerateRandomNumber = selectedNumber => {
   const [generatedNumber, setGeneratedNumber] = useState(null); // Initialize with null or another suitable default value
-
+  const [selectedNumberCopy, setselectedNumberCopy] = useState(selectedNumber);
   const generateNumberFn = sequence => {
     let newGeneratedNumber;
-
     if (sequence === 'greater') {
-      newGeneratedNumber = Math.floor(Math.random() * selectedNumber);
-    } else if (sequence === 'smaller') {
       newGeneratedNumber =
         Math.floor(Math.random() * (selectedNumber + 100)) + selectedNumber;
+    } else if (sequence === 'smaller') {
+      newGeneratedNumber = Math.floor(Math.random() * selectedNumber);
     } else {
       newGeneratedNumber = Math.floor(Math.random() * 100);
     }
-
     // Set the state after evaluating the condition
     setGeneratedNumber(newGeneratedNumber);
   };
-
   return [generatedNumber, generateNumberFn];
 };
